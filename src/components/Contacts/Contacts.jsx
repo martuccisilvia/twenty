94a0b67fbb5974fc3703c58bbec1fd64b7b104fa
martuccisilvia <martuccisilvia.dev@gmail.com>
2025-09-26 00:08:55 +0200

    Contacts Page

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,38 +1,46 @@
 import "./Contacts.css";
 
 const Contacts = () => {
-    return (
+  return (
+    <div className="contacts-container">
+      <div className="contacts-page">
+        <h2>Contact Us</h2>
+        <p>Fill out the form below to send me a message:</p>
 
-        <div className="contacts-container"> 
-            
-            <div className="contacts-page">
-                <h2>Contact Us</h2>
-                <p>Fill out the form below to send me a message:</p>
+        <form
+          action="https://formspree.io/f/xwpqlnqn"
+          method="POST"
+          className="contacts-form"
+        >
+          <label>
+            Name:
+            <input type="text" name="name" required />
+          </label>
 
-                <form
-                    action="https://formspree.io/f/xwpqlnqn"
-                    method="POST"
-                    className="contacts-form"
-                >
-                    <label>
-                        Name:
-                        <input type="text" name="name" required />
-                    </label>
+          <label>
+            Email:
+            <input type="email" name="email" required />
+          </label>
 
-                    <label>
-                        Email:
-                        <input type="email" name="email" required />
-                    </label>
+          <label>
+            Message:
+            <textarea name="message" rows="5" required></textarea>
+          </label>
 
-                    <label>
-                        Message:
-                        <textarea name="message" rows="5" required></textarea>
-                    </label>
+          <button type="submit">Send</button>
+        </form>
 
-                    <button type="submit">Send</button>
-                </form>
-            </div>
-        </div>);
+        <div className="contacts-social">
+          <p>Or you can also find me on social media:</p>
+          <div className="social-links">
+            <a href="https://store.twentyonepilots.com/" target="_blank" rel="noopener noreferrer">Store</a>
+            <a href="https://www.instagram.com/twentyonepilots?igsh=MXBhdjZ0MnR3eno1OQ==" target="_blank" rel="noopener noreferrer">Instagram</a>
+            <a href="https://youtube.com/@twentyonepilots?si=jDlwbCFh6yMZzZ1_" target="_blank" rel="noopener noreferrer">YouTube</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Contacts;
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="footer">
             <p>© 2025 Martucci Silvia. All rights reserved.</p>
             <p>
-                Seguimi sui social:
+                Follow me on social:
                 <a href="https://github.com/martuccisilvia" target="_blank" rel="noopener noreferrer"> GitHub</a>
             </p>
         </footer>
